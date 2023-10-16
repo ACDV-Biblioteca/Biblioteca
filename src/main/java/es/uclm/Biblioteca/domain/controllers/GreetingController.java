af59cbd57511c44a605185b61406a902775b07ef
victorcentellas <victor.centellas@alu.uclm.es>
2023-10-16 15:19:43 +0200

    Esqueleto con principio de jpa

---
+++ src/main/java/es/uclm/Biblioteca/domain/controllers/GreetingController.java
@@ -0,0 +1,32 @@
+package es.uclm.Biblioteca.domain.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import es.uclm.Biblioteca.domain.entities.Greeting;
+import es.uclm.Biblioteca.persistencia.GreetingDAO;
+
+@Controller
+public class GreetingController {
+	private static final Logger log= LoggerFactory.getLogger(GreetingController.class);
+	@Autowired
+	private GreetingDAO greetingDAO;
+	@GetMapping("/greeting") 
+	public String greetingForm(Model model) {
+		//model.addAttribute("greeting", new Greeting());
+		log.info(greetingDAO.findAll().toString());
+		return "greeting"; }
+
+	@PostMapping("/greeting")
+	public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+		model.addAttribute("greeting", greeting);
+		Greeting savedGreeting= greetingDAO.save(greeting); 
+		log.info("Savedgreeting: "+ savedGreeting);
+		return "result";
+	}
+}
