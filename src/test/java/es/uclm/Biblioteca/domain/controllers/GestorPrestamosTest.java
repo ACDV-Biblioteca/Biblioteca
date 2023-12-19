e954b8eaea3399344473a912b0fc2427516804b0
ANTONIO LINUESA ALBARRÁN <Antonio.Linuesa@alu.uclm.es>
2023-12-19 10:36:09 +0100

    Implementacion Test Antonio

--- src/test/java/es/uclm/Biblioteca/domain/controllers/GestorPrestamosTest.java
+++ src/test/java/es/uclm/Biblioteca/domain/controllers/GestorPrestamosTest.java
@@ -1,56 +1,168 @@
 package es.uclm.Biblioteca.domain.controllers;
 
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import es.uclm.Biblioteca.domain.entities.Ejemplar;
+import es.uclm.Biblioteca.domain.entities.Prestamo;
+import es.uclm.Biblioteca.domain.entities.Reserva;
+import es.uclm.Biblioteca.domain.entities.Titulo;
+import es.uclm.Biblioteca.domain.entities.Usuario;
+import es.uclm.Biblioteca.persistencia.AutorDAO;
+import es.uclm.Biblioteca.persistencia.EjemplarDAO;
+import es.uclm.Biblioteca.persistencia.PrestamoDAO;
+import es.uclm.Biblioteca.persistencia.ReservaDAO;
+import es.uclm.Biblioteca.persistencia.TituloAutorDAO;
+import es.uclm.Biblioteca.persistencia.TituloDAO;
+import es.uclm.Biblioteca.persistencia.UsuarioDAO;
+import jakarta.servlet.http.HttpSession;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Before;
 
 public class GestorPrestamosTest {
+	@Mock
+    private EjemplarDAO ejemplarDAO;
+	@Mock
+    private TituloDAO tituloDAO ;
+	@Mock
+    private AutorDAO autorDAO ;
+	@Mock
+    private UsuarioDAO usuarioDAO;
+    @Mock
+    private PrestamoDAO prestamoDAO;
 
-    @Test
-    public void showPrestarEjemplarPageUsuario() {
-    }
+    @Mock
+    private ReservaDAO reservaDAO;
 
-    @Test
-    public void prestarEjemplarUsuario() {
-    }
+    @Mock
+    private TituloAutorDAO tituloAutorDAO;
 
-    @Test
-    public void showPrestarEjemplarPage() {
-    }
+    @InjectMocks
+    private GestorPrestamos gestorPrestamos;
+    
+    private Model model;
+    @Mock
+    private HttpSession session;
 
-    @Test
-    public void prestarEjemplar() {
-    }
+	@Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this); // Inicializar los mocks antes de cada prueba
+        model = mock(Model.class);
 
-    @Test
-    public void mostrarFormularioDevolucionUsuario() {
-    }
+	}
+	@Test
+	public void showPrestarEjemplarPageUsuario() {
+	}
 
-    @Test
-    public void realizarDevolucionUsuario() {
-    }
+	@Test
+	public void prestarEjemplarUsuario() {
+	}
 
-    @Test
-    public void mostrarFormularioDevolucionBibliotecario() {
-    }
+	@Test
+	public void showPrestarEjemplarPage() {
+	}
 
-    @Test
-    public void realizarDevolucionBibliotecario() {
-    }
+	@Test
+	public void testPrestarEjemplar() {
+		// Configurar comportamiento del mock de EjemplarDAO
+        when(ejemplarDAO.findByNoPrestados()).thenReturn(Collections.emptyList());
 
-    @Test
-    public void mostrarListaYFormulario() {
-    }
+        // Llamar al método bajo prueba
+        String resultado = gestorPrestamos.prestarEjemplar(new Prestamo(), "1", 1, mock(Model.class));
 
-    @Test
-    public void procesarEjemplar() {
-    }
+        // Verificar el comportamiento esperado
+        // Ajusta esto según cómo debería comportarse tu método
+        verify(prestamoDAO, never()).save(any(Prestamo.class));
+        assertEquals("PrestarEjemplar", resultado);
+    
+	}
+	private void testRealizarPrestamo() {
+		 // Configurar comportamiento del mock de UsuarioDAO
+        when(usuarioDAO.getById(anyInt())).thenReturn(new Usuario());
 
-    @Test
-    public void mostrarListaYFormularioUsuario() {
-    }
+        // Configurar comportamiento del mock de ReservaDAO
+        when(reservaDAO.deleteByEjemplar(anyInt())).thenReturn(0);
 
-    @Test
-    public void procesarEjemplarUsuario() {
-    }
+        // Llamar al método bajo prueba
+        gestorPrestamos.RealizarPrestamo(new Prestamo(), 1, mock(Model.class), new Ejemplar());
+
+        // Verificar el comportamiento esperado
+        // Ajusta esto según cómo debería comportarse tu método
+        verify(prestamoDAO).save(any(Prestamo.class));
+	}
+	@Test
+	public void mostrarFormularioDevolucionUsuario() {
+	}
+
+	@Test
+	public void realizarDevolucionUsuario() {
+	}
+
+	@Test
+	public void mostrarFormularioDevolucionBibliotecario() {
+	}
+
+	@Test
+	public void realizarDevolucionBibliotecario() {
+	}
+
+	@Test
+	public void mostrarListaYFormulario() {
+	}
+
+	@Test
+	public void testProcesarEjemplar() {
+		// Configurar comportamiento del mock de EjemplarDAO
+        when(ejemplarDAO.findByPrestados()).thenReturn(Collections.emptyList());
+
+        // Llamar al método bajo prueba
+        String resultado = gestorPrestamos.procesarEjemplar(new Reserva(), "1", 1, mock(Model.class));
+
+        // Verificar el comportamiento esperado
+        // Ajusta esto según cómo debería comportarse tu método
+        verify(reservaDAO, never()).save(any(Reserva.class));
+        assertEquals("ReservaEjemplar", resultado);
+	}
+
+	@Test
+	private void testReservarEjemplar() {
+		// Configurar comportamiento del mock de UsuarioDAO
+        when(usuarioDAO.getById(anyInt())).thenReturn(new Usuario());
+
+        // Llamar al método bajo prueba
+        gestorPrestamos.ReservarEjemplar(new Reserva(), 1, new Ejemplar(), mock(Model.class));
+
+        // Verificar el comportamiento esperado
+        // Ajusta esto según cómo debería comportarse tu método
+        verify(reservaDAO).save(any(Reserva.class));
+	}
+
+	@Test
+	public void mostrarListaYFormularioUsuario() {
+	}
+
+	@Test
+	public void procesarEjemplarUsuario() {
+	}
 }
--- src/test/java/es/uclm/Biblioteca/domain/controllers/GestorTitulosTest.java
+++ src/test/java/es/uclm/Biblioteca/domain/controllers/GestorTitulosTest.java
@@ -1,12 +1,63 @@
 package es.uclm.Biblioteca.domain.controllers;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import es.uclm.Biblioteca.domain.entities.Autor;
+import es.uclm.Biblioteca.domain.entities.Ejemplar;
+import es.uclm.Biblioteca.domain.entities.Titulo;
+import es.uclm.Biblioteca.domain.entities.TituloAutor;
+import es.uclm.Biblioteca.persistencia.*;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 public class GestorTitulosTest {
+	@Mock
+    private EjemplarDAO ejemplarDAO;
+	@Mock
+    private TituloDAO tituloDAO ;
+	@Mock
+    private AutorDAO autorDAO ;
 
-    @Test
+    @Mock
+    private PrestamoDAO prestamoDAO;
+
+    @Mock
+    private ReservaDAO reservaDAO;
+
+    @Mock
+    private TituloAutorDAO tituloAutorDAO;
+
+    @InjectMocks
+    private GestorTitulos gestorTitulos;
+        
+    private Model model;
+
+	@Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this); // Inicializar los mocks antes de cada prueba
+        model = mock(Model.class);
+
+    }
+    
+	@Test
     public void showAltaTitulo() {
     }
 
@@ -44,6 +95,28 @@ public class GestorTitulosTest {
 
     @Test
     public void altaEjemplar() {
+    	 // Configurar comportamiento del mock de TituloDAO
+        Long tituloIsbnExistente = 9780312983286L; // ISBN de ejemplo que existe
+        Titulo tituloExistente = new Titulo(); // Ajusta esto según tu implementación real
+        when(tituloDAO.getById(tituloIsbnExistente)).thenReturn(tituloExistente);
+
+        // Llamar al método bajo prueba
+        String resultadoExistente = gestorTitulos.altaEjemplar(new Titulo(), mock(Model.class), tituloIsbnExistente);
+
+        // Verificar que se recibe el mensaje correcto y que se llamó al save de ejemplarDAO
+        verify(ejemplarDAO).save(any(Ejemplar.class));
+        assertEquals("AñadirEjemplar", resultadoExistente);
+
+        // Configurar comportamiento del mock de TituloDAO para un título inexistente
+        Long tituloIsbnNoExistente = 97803129832L; // ISBN de ejemplo que no existe
+        when(tituloDAO.getById(tituloIsbnNoExistente)).thenReturn(null);
+
+        // Llamar al método bajo prueba
+        String resultadoNoExistente = gestorTitulos.altaEjemplar(new Titulo(), mock(Model.class), tituloIsbnNoExistente);
+
+        // Verificar que se recibe el mensaje correcto y que no se llamó al save de ejemplarDAO
+        verify(ejemplarDAO, never()).save(any(Ejemplar.class));
+        assertEquals("AñadirEjemplar", resultadoNoExistente);
     }
 
     @Test
